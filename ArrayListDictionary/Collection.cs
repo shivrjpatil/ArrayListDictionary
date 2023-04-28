288f0662c3590a6f1b563d726127148d1f66e60d
shivrjpatil <shivrajp972@gmail.com>
2023-04-28 20:34:49 +0530

    Added performing crud operations on Arraylist

---
+++ ArrayListDictionary/Collection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+class Collection
+{
+    public void ArrayLit()
+    {
+        // create an ArrayList 
+        ArrayList myArrayList = new ArrayList();
+
+        // add some data to the ArrayList
+        myArrayList.Add("apple");
+        myArrayList.Add("banana");
+        myArrayList.Add("cherry");
+
+        // read data from the ArrayList
+        Console.WriteLine("The first item in the ArrayList is: " + myArrayList[0]);
+
+        // update data in the ArrayList
+        myArrayList[1] = "pear";
+
+        // delete data from the ArrayList
+        myArrayList.RemoveAt(2);
+
+        // print the remaining data in the ArrayList
+        Console.WriteLine("The items in the ArrayList are:");
+        foreach (string item in myArrayList)
+        {
+            Console.WriteLine(item);
+        }
+        Console.ReadLine();
+    }
+}
---
+++ ArrayListDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Collection collection = new Collection();
+            collection.ArrayLit();
+        }
+    }
+}
